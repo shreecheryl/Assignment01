20951c4fdd05bf57523978bde64e1215e837d8f2
shreecheryl <shreecheryl@att.net>
2023-02-10 14:23:07 -0800

    completed assignment

--- js/script.js
+++ js/script.js
@@ -23,8 +23,113 @@
 // let strState
 // let strZipCode
 // let intAge
-// strReferralSource
-// blnMayWeContactYou
+// let strReferralSource
+// let blnMayWeContactYou
 
 // STEP 5
-// 
+// let strFirstName
+// let strLastName
+// let blnMayWeContactYou
+// strFirstName = "Cheryl"
+// strLastName = "Davis"
+// blnMayWeContactYou = true
+
+// let strFirstName = "Cheryl"
+// let strLastName = "Davis"
+// let blnMayWeContactYou = true
+
+// let strFirstName = "Cheryl", strLastName = "Davis", blnMayWeContactYou = true
+
+// STEP 6
+// let myAge = 60
+// console.log("I am " + myAge + " years old.")
+
+// STEP 7
+// let booleanString = true + ": Boolean plus String test"
+// console.log(booleanString)
+
+// let numberBoolean = 42 + true
+// console.log(numberBoolean)
+
+// STEP 8
+// It is not valid. A backlash needs to be added before the apostrophe in I'm so it would be I\'m
+
+// STEP 9
+// let nullVar = null
+// console.log(nullVar)
+
+// let undefinedVar
+// console.log(undefinedVar)
+
+// STEP 10
+// let firstName = "Cheryl"
+// console.log(typeof(firstName))
+// let age = 60
+// console.log(typeof(age))
+// let selfEmployed = true
+// console.log(typeof(selfEmployed))
+// let numbers = [6, 2, 5, 3]
+// console.log(typeof(numbers))
+// let undefinedVar
+// console.log(typeof(undefinedVar))
+
+// STEP 11
+// console.log("Hello " + "Cheryl Davis" + ", welcome to the JavaScript class!")
+
+// STEP 12
+// let myName = "Cheryl Davis"
+// console.log("Hello " + myName + ", welcome to the JavaScript class!")
+
+// STEP 13
+// let myName = "Cheryl Davis"
+// let course = "Javascript"
+// console.log("Hello " + myName + ", welcome to the " + course + " class!")
+
+// STEP 14
+// let myName = "Cheryl Davis"
+// let course = "Javascript"
+// console.log("Hello " + myName + ".\nWelcome to the " + course + " class!")
+
+// STEP 15
+// let namePrompt = prompt("what is your name?")
+// console.log("Hello " + namePrompt + ".\nWelcome to the JavaScript class!")
+
+// STEP 16
+// let namePrompt = prompt("what is your name?")
+// let coursePrompt = prompt("What class are you taking?")
+// console.log("Hello " + namePrompt + ".\nWelcome to the " + coursePrompt + " class!")
+
+// STEP 17
+// let x = 10
+// let y = 20
+// console.log(x + y)
+
+// STEP 18
+// let x = 20
+// x += 20
+// console.log(x)
+
+// STEP 19
+// let x = 20
+// x *= 5
+// console.log(x)
+
+// STEP 20
+// let x = 20 % 3
+// x /= 1
+// console.log(x)
+
+// STEP 21
+// let x = 5
+// let y = 10
+// if (x > 3 && y < 11)
+    // console.log(true)
+
+// STEP 22
+// let x = 5
+// let y = 10
+// if (x === 3 || y != 10)
+    // console.log(true)
+// else
+    // console.log(false)
+    
