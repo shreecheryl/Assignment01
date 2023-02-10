5042289307b2f50312ddf2b1fc4ed4d0864f01de
shreecheryl <shreecheryl@att.net>
2023-02-10 11:29:09 -0800

    thru Step 4 completed

--- js/script.js
+++ js/script.js
@@ -5,3 +5,26 @@
 // let summerMonth
 // let myLibraryFunction
 
+// STEP 2
+// 42   // numeric literal express
+// "hello, world"   // string literal expression
+// true // boolean literal expression
+// null // null literal expression
+
+// STEP 3
+// let greeting = "hello, world"
+// let add = x + y
+
+// STEP 4
+// let strFirstName
+// let strLastName
+// let strAddress
+// let strCity
+// let strState
+// let strZipCode
+// let intAge
+// strReferralSource
+// blnMayWeContactYou
+
+// STEP 5
+// 
